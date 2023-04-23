f5ae69f8fe06edc2788b89e11ed585bd0c503b7c
Byron Ochieng <byronochieng01@gmail.com>
2023-04-23 23:07:00 +0300

    manifest correction

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,11 +5,13 @@ include stdlib
 File_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  content   => "PasswordAuthentication no\n"
+  line   => '    PasswordAuthentication no',
+  replace => true
 }
 
 File_line { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  content   => "IdentityFile ~/.ssh/school\n"
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true
 }
